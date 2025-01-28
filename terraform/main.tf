6943fb9dfc1a92a8775c19f0e93e31b110365e10
frocode <frocodeof@gmail.com>
2025-01-28 22:55:56 +0100

    ++

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,12 +10,11 @@ resource "aws_glue_catalog_database" "spark_streaming_db" {
 # Glue Crawler
 resource "aws_glue_crawler" "spark_streaming" {
   name          = "spark_streaming_crawler"
-  role          = aws_iam_role.glue_crawler_role.arn # Replace with your IAM role ARN
+  role          = aws_iam_role.glue_crawler_role.arn # IAM role ARN
   database_name = aws_glue_catalog_database.spark_streaming_db.name
 
   s3_target {
     path = "s3://sparkbucketfrocode/streaming/streaming-output/"
-
     exclusions = [
       "_spark_metadata/"
     ]
@@ -49,60 +48,29 @@ resource "aws_iam_role" "glue_crawler_role" {
     ]
   })
 }
-# IAM policy for Glue catalog access
-resource "aws_iam_policy" "glue_crawler_catalog_policy" {
-  name        = "glue_crawler_catalog_policy"
-  description = "Policy for Glue crawler to access Glue catalog database"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action   = [
-          "glue:GetDatabase",
-          "glue:GetTable",
-          "glue:GetTableVersion",
-          "glue:GetTables"
-        ],
-        Effect   = "Allow",
-        Resource = "arn:aws:glue:eu-central-1:905418126921:catalog"
-      }
-    ]
-  })
-}
+# Attach AWS managed policies to the Glue crawler role
 
-# Attach the Glue catalog policy to the Glue crawler role
-resource "aws_iam_role_policy_attachment" "glue_crawler_catalog_policy_attachment" {
+# Attach AmazonS3FullAccess
+resource "aws_iam_role_policy_attachment" "glue_crawler_s3_full_policy" {
   role       = aws_iam_role.glue_crawler_role.name
-  policy_arn = aws_iam_policy.glue_crawler_catalog_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-
-# IAM policy for S3 access
-resource "aws_iam_policy" "glue_crawler_s3_policy" {
-  name        = "glue_crawler_s3_policy"
-  description = "Policy for Glue crawler to access S3 bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action   = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Effect   = "Allow",
-        Resource = [
-          "arn:aws:s3:::sparkbucketfrocode/streaming/streaming-output/*",
-          "arn:aws:s3:::sparkbucketfrocode"
-        ]
-      }
-    ]
-  })
-}
-
-# Attach policy to role
-resource "aws_iam_role_policy_attachment" "glue_crawler_role_policy_attachment" {
+# Attach AWSGlueConsoleFullAccess
+resource "aws_iam_role_policy_attachment" "glue_crawler_console_full_policy" {
   role       = aws_iam_role.glue_crawler_role.name
-  policy_arn = aws_iam_policy.glue_crawler_s3_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"
+}
+
+# Attach AmazonRedshiftFullAccess
+resource "aws_iam_role_policy_attachment" "glue_crawler_redshift_full_policy" {
+  role       = aws_iam_role.glue_crawler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRedshiftFullAccess"
+}
+
+# Attach AmazonRedshiftDataFullAccess
+resource "aws_iam_role_policy_attachment" "glue_crawler_redshift_data_full_policy" {
+  role       = aws_iam_role.glue_crawler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRedshiftDataFullAccess"
 }
