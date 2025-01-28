af12f09517ddc9b73dd0d2179dbd6b47dc6cf8c0
frocode <frocodeof@gmail.com>
2025-01-28 23:34:40 +0100

    ++

--- config/config.py
+++ config/config.py
@@ -2,6 +2,9 @@
 from dotenv import load_dotenv
 import os
 
+dotenv_path = os.getenv("ENV_FILE_PATH")
+load_dotenv(dotenv_path=dotenv_path)
+
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
@@ -9,3 +12,5 @@ configuration = {
     'AWS_ACCESS_KEY': AWS_ACCESS_KEY,
     'AWS_SECRET_KEY': AWS_SECRET_KEY
 }
+print(f"API Key: {AWS_ACCESS_KEY}")
+print(f"AWS_SECRET_KEY URL: {AWS_SECRET_KEY}")
